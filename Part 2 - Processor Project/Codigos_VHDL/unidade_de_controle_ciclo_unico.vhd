122edf10daf18b73b72e2ec80531c91f4529714d
André Lamego <andrelamegobh@hotmail.com>
2024-06-29 11:16:58 -0300

    Unidade de controle finalizada

--- Part 2 - Processor Project/Codigos_VHDL/unidade_de_controle_ciclo_unico.vhd
+++ Part 2 - Processor Project/Codigos_VHDL/unidade_de_controle_ciclo_unico.vhd
@@ -13,12 +13,13 @@ entity unidade_de_controle_ciclo_unico is
     generic (
         INSTR_WIDTH       : natural := 32;
         OPCODE_WIDTH      : natural := 5;
-        DP_CTRL_BUS_WIDTH : natural := 6;
-        ULA_CTRL_WIDTH    : natural := 4
+        DP_CTRL_BUS_WIDTH : natural := 7;
+        ULA_CTRL_WIDTH    : natural := 5
     );
     port (
-        instrucao : in std_logic_vector(INSTR_WIDTH - 1 downto 0);       -- instrução
-        controle  : out std_logic_vector(DP_CTRL_BUS_WIDTH - 1 downto 0) -- controle da via
+        instrucao : in std_logic_vector(INSTR_WIDTH - 1 downto 0);          -- instrução
+        controle  : out std_logic_vector(DP_CTRL_BUS_WIDTH - 1 downto 0)    -- controle da via
+        alu_ctrl  : out std_logic_vector(ULA_CTRL_WIDTH - 1 downto 0)       -- Controle da ALU
     );
 end unidade_de_controle_ciclo_unico;
 
@@ -28,37 +29,141 @@ architecture beh of unidade_de_controle_ciclo_unico is
     --signal opcode   : std_logic_vector (OPCODE_WIDTH-1 downto 0);			-- opcode
     --signal ctrl_aux : std_logic_vector (DP_CTRL_BUS_WIDTH-1 downto 0);		-- controle
 
-    signal inst_aux : std_logic_vector (15 downto 0); -- instrucao
-    signal opcode   : std_logic_vector (3 downto 0);  -- opcode
-    signal ctrl_aux : std_logic_vector (5 downto 0);  -- controle
+    signal inst_aux : std_logic_vector (31 downto 0); -- instrucao
+    signal opcode   : std_logic_vector (4 downto 0);  -- opcode
+    signal ctrl_aux : std_logic_vector (6 downto 0);  -- controle
+    signal ctrl_ula : std_logic_vector (4 downto 0);  -- controle da alu
 
 begin
     inst_aux <= instrucao;
     -- A linha abaixo não produz erro de compilação no Quartus II, mas no Modelsim (GHDL) produz.	
     --	opcode <= inst_aux (INSTR_WIDTH-1 downto INSTR_WIDTH-OPCODE_WIDTH);
-    opcode <= inst_aux (15 downto 12);
+    opcode <= inst_aux (31 downto 27);
 
     process (opcode)
     begin
         case opcode is
-                -- NAND	
-            when "0000" =>
-                ctrl_aux <= "110000";
-                -- OR
-            when "0001" =>
-                ctrl_aux <= "110001";
-                -- ADD
-            when "0010" =>
-                ctrl_aux <= "110010";
+                -- ADD	
+            when "00001" =>
+                ctrl_aux <= "0100000";
+                ctrl_ula <= "00000";
+            
+                -- ADDI	
+            when "00010" =>
+                ctrl_aux <= "0101000";
+                ctrl_ula <= "00000";
+            
                 -- SUB	
-            when "0100" =>
-                ctrl_aux <= "110100";
-                -- XOR
-            when "1100" =>
-                ctrl_aux <= "111100";
+            when "00011" =>
+                ctrl_aux <= "0100000";
+                ctrl_ula <= "00001";
+            
+                -- MUL	
+            when "00100" =>
+                ctrl_aux <= "0100000";
+                ctrl_ula <= "00010";
+            
+                -- DIV
+            when "00101" =>
+                ctrl_aux <= "0100000";
+                ctrl_ula <= "00011";
+            
+                -- AND	
+            when "00110" =>
+                ctrl_aux <= "0100000";
+                ctrl_ula <= "01010";
+            
+                -- OR	
+            when "00111" =>
+                ctrl_aux <= "0100000";
+                ctrl_ula <= "01011";
+            
+                -- ANDI	
+            when "01000" =>
+                ctrl_aux <= "0101000";
+                ctrl_ula <= "01010";
+            
+                -- SLL	
+            when "01001" =>
+                ctrl_aux <= "0101000";
+                ctrl_ula <= "01100";
+            
+                -- SRL	
+            when "01010" =>
+                ctrl_aux <= "0101000";
+                ctrl_ula <= "01101";
+            
+                -- LW	
+            when "01011" =>
+                ctrl_aux <= "0101001";
+                ctrl_ula <= "00000";
+            
+                -- SW	
+            when "01100" =>
+                ctrl_aux <= "00x1010";
+                ctrl_ula <= "00000";
+            
+                -- DIVC	
+            when "01101" =>
+                ctrl_aux <= "0100000";
+                ctrl_ula <= "00111";
+            
+                -- BEQ	
+            when "01110" =>
+                ctrl_aux <= "00x1100";
+                ctrl_ula <= "01110";
+            
+                -- BLT	
+            when "01111" =>
+                ctrl_aux <= "00x1100";
+                ctrl_ula <= "10000";
+            
+                -- BGT	
+            when "10000" =>
+                ctrl_aux <= "00x1100";
+                ctrl_ula <= "01111";
+            
+                -- JMP	
+            when "10001" =>
+                ctrl_aux <= "10xx000";
+                ctrl_ula <= "xxxxx";
+            
+                -- JAL	
+            when "10010" =>
+                ctrl_aux <= "111x000";
+                ctrl_ula <= "xxxxx";
+            
+                -- ADDC	
+            when "10011" =>
+                ctrl_aux <= "0100000";
+                ctrl_ula <= "00100";
+            
+                -- SUBC	
+            when "10100" =>
+                ctrl_aux <= "0100000";
+                ctrl_ula <= "00101";
+            
+                -- MULC	
+            when "10101" =>
+                ctrl_aux <= "0100000";
+                ctrl_ula <= "00110";
+            
+                -- ABS	
+            when "10110" =>
+                ctrl_aux <= "010x000";
+                ctrl_ula <= "01000";
+            
+                -- CONJ	
+            when "10111" =>
+                ctrl_aux <= "010x000";
+                ctrl_ula <= "01001";
+            
             when others =>
                 ctrl_aux <= (others => '0');
+                ctrl_ula <= (others => '0');
         end case;
     end process;
     controle <= ctrl_aux;
+    alu_ctrl <= ctrl_ula;
+    
 end beh;
